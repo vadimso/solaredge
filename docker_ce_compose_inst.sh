c76779ca44bf9da0869cee99dad69a21385a2adb
vadimso <so.vadim@gmail.com>
2018-09-14 20:25:38 +0300

    Create docker_ce_compose_inst.sh

---
+++ docker_ce_compose_inst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+docker --version
+docker-compose --version
